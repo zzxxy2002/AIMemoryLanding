48c0c696cf74386217e829c80a7ba0510357baba
Xiangyu Xu <xiangyu.xu@berkeley.edu>
2024-11-10 15:38:57 +0800

    go

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import App from "./FeatureSection/FeatureSection.jsx";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <FeatureSection />
   </React.StrictMode>
 );
