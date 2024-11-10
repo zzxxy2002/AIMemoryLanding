8217768de99e0a5c2fd8098adab428b9080f8def
Xiangyu Xu <xiangyu.xu@berkeley.edu>
2024-11-10 15:57:33 +0800

    main page done

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./FeatureSection/FeatureSection.jsx";
+import InThisMoment from "./InThisMoment/InThisMoment.jsx";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
@@ -8,6 +8,6 @@ Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FeatureSection />
+    <InThisMoment />
   </React.StrictMode>
 );
